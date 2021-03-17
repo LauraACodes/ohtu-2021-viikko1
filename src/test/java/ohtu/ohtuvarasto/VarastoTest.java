f4836d9f97881327bb99d9d50d712517eebc0a02
Laura Andstrom <laura.andstrom@helsinki.fi>
2021-03-17 11:39:58 +0200

    JUnit testit luotu, rivikattavuus 100

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -19,12 +19,54 @@ public class VarastoTest {
     public void setUp() {
         varasto = new Varasto(10);
     }
+    
+    @Test
+    public void kayttokelvotottomanVarastontilavuusNolla() {
+       Varasto olematonVarasto = new Varasto(0);
+       assertEquals(0, olematonVarasto.getTilavuus(),vertailuTarkkuus); 
+    }
+    
+    @Test
+    public void kayttokelvotottomanSaldollisenVarastontilavuusNolla() {
+       Varasto olematonVarasto = new Varasto(0,0);
+       assertEquals(0, olematonVarasto.getTilavuus(),vertailuTarkkuus); 
+    }
+
+    @Test
+    public void kayttokelvotottomanSaldollisenVarastonSaldoNolla() {
+       Varasto olematonVarasto = new Varasto(0,0);
+       assertEquals(0, olematonVarasto.getSaldo(),vertailuTarkkuus); 
+    }
 
     @Test
     public void konstruktoriLuoTyhjanVaraston() {
         assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
     }
 
+    @Test
+    public void alkuSaldoaSisaltavaKonstruktoriLuoOikeanSaldoisenVaraston() {
+        Varasto saldoinenVarasto = new Varasto(20,5);
+        assertEquals(5, saldoinenVarasto.getSaldo(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void alkuSaldoaSisaltavaKonstruktoriLuoOikeanTilavuudenVarastoon() {
+        Varasto saldoinenVarasto = new Varasto(20,5);
+        assertEquals(20, saldoinenVarasto.getTilavuus(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void alkuSaldoaSisaltavaKonstruktoriEiAnnaLuodaNegSaldoa() {
+        Varasto saldoinenVarasto = new Varasto(20,-5);
+        assertEquals(0, saldoinenVarasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void alkuSaldoaSisaltavaKonstruktoriEiAnnaLuodaLiianSuurtaSaldoa() {
+        Varasto saldoinenVarasto = new Varasto(20,25);
+        assertEquals(20, saldoinenVarasto.getSaldo(), vertailuTarkkuus);
+    }    
+    
     @Test
     public void uudellaVarastollaOikeaTilavuus() {
         assertEquals(10, varasto.getTilavuus(), vertailuTarkkuus);
@@ -37,7 +79,24 @@ public class VarastoTest {
         // saldon pitäisi olla sama kun lisätty määrä
         assertEquals(8, varasto.getSaldo(), vertailuTarkkuus);
     }
+    
+    @Test
+    public void negatiivinenlisaysEiLisaaSaldoa() {
+        varasto.lisaaVarastoon(10); //laitetaan aluksi jotain, että voidaan verrata
+        varasto.lisaaVarastoon(-5);
 
+        // saldon pitäisi olla sama kun lisätty määrä
+        assertEquals(10, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void liikalisaysEiLisaaSaldoaYliTilavuuden() {
+        varasto.lisaaVarastoon(15); // yritetään laittaa 5 enemmän kuin tilavuus
+
+        // saldon pitäisi olla silti 10 eli max tilavuus
+        assertEquals(10, varasto.getSaldo(), vertailuTarkkuus);
+    }    
+    
     @Test
     public void lisaysLisaaPienentaaVapaataTilaa() {
         varasto.lisaaVarastoon(8);
@@ -56,6 +115,24 @@ public class VarastoTest {
     }
 
     @Test
+    public void eiSaaOttaaNegatiivista() {
+        varasto.lisaaVarastoon(5);
+
+        double saatuMaara = varasto.otaVarastosta(-2);
+
+        assertEquals(0, saatuMaara, vertailuTarkkuus);
+    }
+    
+    @Test
+    public void eiSaaOttaaEnemmanKuinVarastossaOn() {
+        varasto.lisaaVarastoon(5);
+
+        double saatuMaara = varasto.otaVarastosta(10);
+
+        assertEquals(5, saatuMaara, vertailuTarkkuus);
+    }   
+    
+    @Test
     public void ottaminenLisääTilaa() {
         varasto.lisaaVarastoon(8);
 
@@ -64,5 +141,17 @@ public class VarastoTest {
         // varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
+
+    @Test
+    public void stringTulostusOnOikein() {
+        varasto.lisaaVarastoon(2);
+        
+        assertEquals("saldo = 2.0, vielä tilaa 8.0", varasto.toString());
+    }
+    
+    private void assertEqual(int i, double tilavuus) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
 
 }
